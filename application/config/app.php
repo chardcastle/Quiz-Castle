d3aef597e01d1bff626a5f8baa6fed14edb0b5c9
Chris Hardcastle (Saturn) <me@chrishardcastle.co.uk>
2011-08-14 00:35:15 +0100

    Moving items around in folder for ease and config.

--- application/config/app.php
+++ application/config/app.php
@@ -12,6 +12,18 @@ return array(
 	 *  * STAGING
 	 *  * PRODUCTION
 	 */
-	'environment'  => Kohana::DEVELOPMENT,
-	'is_form_active' => true,
+	'environment'  => Kohana::DEVELOPMENT,	
+	'facebook' => array(
+		'id_key' => '220812297965188',
+		'secret' => '2dfa123a305720e004daeade49e51b3a',	
+		'canvas_url' => 'http://apps.facebook.com/emc_contest',
+		'tab_page_url' => 'http://www.facebook.com/pages/EMC-Contest/181942355205924?sk=app_220812297965188',	
+	),
+	'app_url' => 'http://quizcastle.local.net',
+	'home_address' => 'http://chrishardcastle.co.uk',
+	'admins' => array(
+		'664739612',
+		'523155273',
+		'879870713',
+	),
 );
