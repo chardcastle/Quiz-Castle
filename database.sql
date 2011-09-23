106572cc94bad58d6cec481e6e41fd276e2dd7f5
Chris Hardcastle (Saturn) <me@chrishardcastle.co.uk>
2011-09-23 23:58:12 +0100

    Updated database schema.

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.14, for osx10.6 (i386)
+-- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (i486)
 --
--- Host: localhost    Database: toblerone_recipes
+-- Host: localhost    Database: quizcastle
 -- ------------------------------------------------------
--- Server version	5.5.14
+-- Server version	5.1.41-3ubuntu12.10
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,35 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `scores`
+--
+
+DROP TABLE IF EXISTS `scores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scores` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `score` varchar(200) DEFAULT '',
+  `score_breakdown` varchar(225) DEFAULT NULL,
+  `submitted` datetime DEFAULT NULL,
+  `entry_token` text,
+  `question_ids` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scores`
+--
+
+LOCK TABLES `scores` WRITE;
+/*!40000 ALTER TABLE `scores` DISABLE KEYS */;
+INSERT INTO `scores` VALUES (1,98467,'6','a:2:{i:0;s:6:\"fafasd\";i:1;s:9:\"fsdfsdasd\";}','2011-09-06 11:42:13','fake','1,9,8,7,2,6,5,3,0,4'),(2,98467,'6','a:2:{i:0;s:6:\"fafasd\";i:1;s:9:\"fsdfsdasd\";}','2011-09-06 11:42:13','fake','1,9,8,7,2,6,5,3,0,4'),(3,98467,'6','a:2:{i:0;s:6:\"fafasd\";i:1;s:9:\"fsdfsdasd\";}','2011-09-06 11:42:13','fake','1,9,8,7,2,6,5,3,0,4'),(4,0,'299','a:10:{i:1;b:1;i:9;b:0;i:8;b:0;i:7;b:1;i:2;b:0;i:6;b:0;i:5;b:0;i:3;b:1;i:0;b:0;i:4;b:0;}','2011-09-23 17:55:47','b56150e25ffb85a616872b346ddbe568','1,9,8,7,2,6,5,3,0,4');
+/*!40000 ALTER TABLE `scores` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -52,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-08-19 10:18:23
+-- Dump completed on 2011-09-23 23:58:02
